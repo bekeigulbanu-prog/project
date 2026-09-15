59df1844a520263430ce4028c88399c0d9bc5ed2
bekeigulbanu-prog <bekeigulbanu@gmail.com>
2026-09-15 17:21:34 +0500

    цувгшаышварг

--- shared.js
+++ shared.js
@@ -1,14 +1,11 @@
-// Находим элементы для Гульбану
 const cardGulbanu = document.getElementById('open-gulbanu');
 const modalGulbanu = document.getElementById('modal-gulbanu');
 const closeGulbanu = document.getElementById('close-gulbanu');
 
-// Находим элементы для Напарницы
 const cardPartner = document.getElementById('open-partner');
 const modalPartner = document.getElementById('modal-partner');
 const closePartner = document.getElementById('close-partner');
 
-// Открытие окна Гульбану
 cardGulbanu.addEventListener('click', () => {
     modalGulbanu.style.display = 'flex';
 });
@@ -16,7 +13,6 @@ closeGulbanu.addEventListener('click', () => {
     modalGulbanu.style.display = 'none';
 });
 
-// Открытие окна Напарницы
 cardPartner.addEventListener('click', () => {
     modalPartner.style.display = 'flex';
 });
@@ -24,7 +20,6 @@ closePartner.addEventListener('click', () => {
     modalPartner.style.display = 'none';
 });
 
-// Закрытие по клику вне модального окна
 window.addEventListener('click', (event) => {
     if (event.target === modalGulbanu) {
         modalGulbanu.style.display = 'none';
